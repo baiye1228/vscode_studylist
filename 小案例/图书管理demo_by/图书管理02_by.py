a53c080861e1b618ed3037a6e8e3c5af06acbe3c
baiye <baiyebzx1228@gmail.com>
2020-09-18 17:41:45 +0800

    小更改

---
+++ 小案例/图书管理demo_by/图书管理02_by.py
@@ -0,0 +1,20 @@
+import os
+
+user_path=r'F:\vscode\code_list\study_list\小案例\图书管理demo_by\用户资料_by\user.txt'
+books_path=r'F:\vscode\code_list\study_list\小案例\图书管理demo_by\图书资料_by'
+
+class Library:
+    def __init__(self):
+        
+
+class Visit:
+    pass
+
+class User(Library):
+    pass
+
+class Manager(Library):
+    pass
+
+class book:
+    pass
